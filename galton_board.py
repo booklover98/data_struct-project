6f5ff13b4352a125cfa693b013909b4fd37e0915
booklover98 <samantha.husack@uoit.net>
2018-03-29 17:22:21 -0400

    update

--- galton_board.py
+++ galton_board.py
@@ -1,43 +1,47 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 execute = True
 
 while execute:
-
-    b_length = 20
+    
+    n = int(input("Enter number of balls: "))
+    length = 6
     num_balls = n
-    output = [0] * (b_length + 1)
+    output = [0] * (length+1)
 
     def ball():
         path = ""
-        for a in range(0, b_length):
+        for a in range(0, length):
             direction = random.choice("LR")
             path += direction
         return path
 
     def location(str):
-        x = [0]*(b_length + 1)
+        x = [0]*(length + 1)
         A = str.count("L")
         B = str.count("R")
         if ((A - B) > 0):
                 total = A - B
-                x[int(b_length/2+total/2)] += 1
+                x[int(length/2 - total/2)] += 1
         elif ((B - A) > 0):
                 total = B - A
-                x[int(b_length/2+total/2)] += 1
+                x[int(length/2 + total/2)] += 1
         else:
-                x[int(b_length/2)] += 1
+                x[int(length/2)] += 1
         return x
 
-    n = int(input("Enter number of balls: "))
     while num_balls > 0:
-            ##print evolving 2d array
-            ##loop with an exit case
-            ##while running the program, or the trial is done, 
-            ##update, print, update condition, have a break
             placement = location(ball())
             output = [x + y for x, y in zip(output, location(ball()))]
             num_balls -= 1
+            plt.hist(output)
+            plt.title("Galton Board")
+            plt.show()
+
+    print(output)
+    plt.show()
 
     if input("Calculate another board? [y/n]") == "n":
         execute = False
