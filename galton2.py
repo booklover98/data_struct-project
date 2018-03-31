eaff2307fe1c9ee0f70922528845d3c8627d0baf
Ari Lotter <arilotter@gmail.com>
2018-03-30 21:20:59 -0400

    update galton simulation

--- galton2.py
+++ galton2.py
@@ -4,38 +4,50 @@ import matplotlib.animation as animation
 
 import random
 
-board_size = 7
+BOARD_SIZE = 7
+VIDEO_LENGTH = 10  # seconds
+FPS = 30
+
 
 def get_ball_bucket():
-    return [random.choice("LR") for _ in range(board_size)].count("R")
+    return [random.choice("LR") for _ in range(BOARD_SIZE)].count("R")
 
-def galton_animate(n):
+
+def galton_animate(num_balls):
     fig, ax = plt.subplots()
 
-    # simulation = [get_ball_bucket() for _ in range(n)]
-    x = range(0, board_size + 1)
-    simulation = [0] * (board_size + 1)
-    plot = plt.bar (x, simulation)
+    x = range(0, BOARD_SIZE + 1)
+    simulation = [0] * (BOARD_SIZE + 1)
+    plot = plt.bar(x, simulation)
+    ball_counter = 0
+    frames = VIDEO_LENGTH * FPS
 
+    balls_per_frame = max(num_balls // frames, 1)
+    ax.annotate("balls: " + str(num_balls), xy=(0.75, 0.9), xycoords='axes fraction', fontsize=10,
+                bbox=dict(facecolor='white', alpha=0.8),
+                horizontalalignment='left', verticalalignment='bottom')
     def animate(i):
-        if i % 1000 == 0:
-            print(i/n)
-        if i < n:
-            simulation[get_ball_bucket()] += 1 # drop a random ball
+        nonlocal ball_counter
+        if ball_counter < num_balls:
+            for _ in range(balls_per_frame):
+                if ball_counter >= num_balls:
+                    return
+                simulation[get_ball_bucket()] += 1  # drop a random ball
+                ball_counter += 1
         for bar, y in zip(plot, simulation):
             bar.set_height(y)
         ax.relim()
-        ax.autoscale_view(True,True,True)
+        ax.autoscale_view(True, True, True)
 
-
-    ani = animation.FuncAnimation(fig, animate, n + 10,
-                                interval=500/n, blit=False, repeat=False)
+    ani = animation.FuncAnimation(fig, animate, frames + FPS,
+                                  interval=1/FPS, blit=False, repeat=False)
     # Set up formatting for the movie files
     Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+    writer = Writer(fps=FPS, metadata=dict(artist='Me'), bitrate=1800)
 
-    ani.save(str(n) + '_balls.mp4', writer=writer)
-    print("Done simulation for " + str(n) + " balls!")
+    ani.save(str(num_balls) + '_balls.mp4', writer=writer)
+    print("Done simulation for " + str(num_balls) + " balls!")
+
 
 if __name__ == "__main__":
     for n in [100, 1000, 10000, 50000, 100000, 1000000]:
