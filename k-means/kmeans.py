0faca99bea3e617a93843c149fe3a6403052bee2
Sunnypatel1 <sunnypatel1@live.com>
2018-04-02 17:43:04 -0400

    Updated GUI and program

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -15,9 +15,10 @@ class Cluster:
     def addPoints (self, point):
         self.points.append(point)
     def backup(self):
-        self.prevPoints = self.points
+        self.prevPoints = list(self.points)
         self.points = []
     def checkChanged(self):
+        if (len(self.prevPoints) == 0): return False
         return set(self.prevPoints) == set(self.points)
 
 def assignToCluster(clusters, dataset):
@@ -25,31 +26,32 @@ def assignToCluster(clusters, dataset):
     
     for point in dataset:
         minDist = euclDistGeneral(point, clusters[0].mean)
-        minCluster = None 
+        minCluster = clusters[0] 
 
         for cluster in clusters:
             if euclDistGeneral(point, cluster.mean) <= minDist:
                 minDist = euclDistGeneral(point, cluster.mean);
                 minCluster = cluster
-        minCluster.addPoints(point)
+        minCluster.points.append(point)
 
         
     for cluster in clusters:
 
         changeOccured = cluster.checkChanged()
 
-        if (changeOccured):
+        if (not changeOccured):
             converged = False
-        
     return converged
 
 def shiftMeans(clusters):
     
     for cluster in clusters:
-        average = (0, 0)
+        average = (0, 0) if len(cluster.mean) == 2 else (0, 0, 0)
         for point in cluster.points:
             average = tuple(map(add, point, average))
         average = tuple(map(lambda x : x/len(cluster.points), average))
+        cluster.mean = average
+        cluster.backup()
             
 
 def euclDist2d(pointA, pointB):
@@ -62,7 +64,7 @@ def euclDistGeneral (pointA, pointB):
     sum = 0
     for i in range (0, len(pointA)):
         sum += (pointA[i] - pointB[i])**2
-    return math.sqrt(sum)
+    return sum
 
 def runCSV (k, CSVString, dim):
     dataset = []
@@ -83,6 +85,9 @@ def runCSV (k, CSVString, dim):
     for i in range (0, k):
         clusters.append( Cluster(dataset[randint(0, len(dataset) - 1)]) )
 
+    print(len(dataset))
+    print(len(clusters))
+
     runAlgo(clusters, dataset)
         
     for x in range (0, len(clusters)):
@@ -96,9 +101,10 @@ def runRand (k, numPoints, dim):
 
     dataset = []
     clusters = []
-    colors = ['b.', 'g.', 'r.', 'c.', 'y.', 'm.']
+    
 
     if (dim == 2):
+        colors = ['b.', 'g.', 'r.', 'c.', 'y.', 'm.']
         for i in range (0, numPoints):
             dataset.append( (randint(0, 200), randint(0, 200)) )
 
@@ -117,14 +123,13 @@ def runRand (k, numPoints, dim):
         plt.show()
                     
     elif (dim == 3):
+        colors = ['b','g','r','c','y','m']
         for i in range (0, numPoints):
             dataset.append( (randint(0, 200), randint(0, 200), randint(0, 200)) )
-        print(dataset)
 
         # Random assignment
         for i in range (0, k):
             clusters.append( Cluster(dataset[randint(0, len(dataset) - 1)]) )
-        print(len(clusters))
 
         runAlgo(clusters, dataset)
 
@@ -132,8 +137,9 @@ def runRand (k, numPoints, dim):
         ax = fig.add_subplot(111, projection='3d')
 
         for x in range (0, len(clusters)):
+            print("one cluster down")
             for point in clusters[x].points:
-                ax.scatter(point[0], point[1], point[2], colors[x])
+                ax.scatter(point[0], point[1], point[2], c=colors[x])
 
         return plt
 
@@ -142,7 +148,8 @@ def runAlgo(clusters, dataset):
 
     while  not converged:
         converged = assignToCluster(clusters, dataset)
-        shiftMeans(clusters)
+        if (not converged):
+            shiftMeans(clusters)
 
     
 if __name__ == "__main__":
